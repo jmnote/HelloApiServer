b8bd032f2ff266ea687621004c15f87cff9ea274
jmnote <opcore@gmail.com>
2017-04-07 15:15:57 +0900

    Spring Starter Project with Gradle

---
+++ src/main/java/com/jmnote/HelloApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.jmnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApiServerApplication.class, args);
+	}
+}
