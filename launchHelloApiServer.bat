abbe0244f6e077fefc476ee20314504bf5d90465
jmnote <opcore@gmail.com>
2017-04-07 17:37:56 +0900

    some config

---
+++ launchHelloApiServer.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar build\libs\HelloApiServer-0.0.1-SNAPSHOT.jar
+
--- src/main/java/com/jmnote/HelloApiServerApplication.java
+++ src/main/java/com/jmnote/HelloApiServerApplication.java
@@ -1,12 +1,23 @@
 package com.jmnote;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApiServerApplication {
+	
+	//@Value("${logfilename}")
+	//private String logfilename;
 
 	public static void main(String[] args) {
+//		try {
+//			System.setOut(new PrintStream(new FileOutputStream("log.txt",true)));
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.print("hh--");
+		
 		SpringApplication.run(HelloApiServerApplication.class, args);
 	}
 }
